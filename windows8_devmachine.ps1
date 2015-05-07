fdd042b116d8ff3f51cdaa39a91ce8cc9ee0aac1
Fábio André Damas <skkeeper@gmail.com>
2015-05-07 22:33:38 +0100

    Add irfanviewplugins, everything, smartgit, sharex and unchecky

--- windows8_devmachine.ps1
+++ windows8_devmachine.ps1
@@ -1,8 +1,14 @@
-# http://boxstarter.org/package/url?
+# Tested on Windows 8.1
 
-$Boxstarter.RebootOk=$true # Allow reboots?
+# Commandline: START http://boxstarter.org/package/nr/url?
+# Internet Explorer: http://boxstarter.org/package/url?
+
+# The following settings will ask you for your windows password and then
+# successfuly reboot the machine everytime it needs to. After Boxstarter is
+# done autologin won't be enabled.
+$Boxstarter.RebootOk=$true    # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
-$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+$Boxstarter.AutoLogin=$true   # Save my password securely and auto-login after a reboot
 
 # Allow running PowerShell scripts
 Update-ExecutionPolicy Unrestricted
@@ -10,12 +16,13 @@ Update-ExecutionPolicy Unrestricted
 Set-TaskbarSmall
 Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableShowAppsViewOnStartScreen -EnableSearchEverywhereInAppsView -EnableListDesktopAppsFirst
+
+# Install Windows Update and reboot
 Install-WindowsUpdate -acceptEula
 if (Test-PendingReboot) { Invoke-Reboot }
 
 #region WindowsExtras
   cinst -y TelnetClient -source windowsFeatures
-  cinst -y IIS-HttpCompressionDynamic -source windowsfeatures
 #endregion
 
 #region DotNetAndPowershell
@@ -38,12 +45,14 @@ if (Test-PendingReboot) { Invoke-Reboot }
   cinst -y 7zip.install
   cinst -y sysinternals
   cinst -y irfanview
+  cinst -y irfanviewplugins
   cinst -y sumatrapdf
   cinst -y mpc-hc
   cinst -y ccleaner
   cinst -y defraggler
-  cinst -y treesizefree #this was outdated
+  cinst -y treesizefree
   cinst -y classic-shell
+  cinst -y everything
 #endregion
 
 #region Browsers
@@ -60,10 +69,11 @@ if (Test-PendingReboot) { Invoke-Reboot }
   cinst -y pycharm-community
   cinst -y androidstudio
   cinst -y winmerge
+  cinst -y smartgit
 #endregion
 
 #region games
-  cinst -y steam
+  #cinst -y steam
 #endregion
 
 #region goingIntoTheExtras
@@ -71,7 +81,11 @@ if (Test-PendingReboot) { Invoke-Reboot }
   cinst -y libreoffice
   cinst -y foxitreader
   cinst -y Paint.net
+  cinst -y sharex
+  cinst -y unchecky
 #endregion
 
+# Make sure some windows update didn't creep on us after installing all
+# those apps
 Install-WindowsUpdate -acceptEula
 if (Test-PendingReboot) { Invoke-Reboot }
